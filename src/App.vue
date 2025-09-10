6a29bad4ba0e4b3f3d21369f04ff09bcdcd67279
Stadnik <stadnik.email@gmail.com>
2025-09-10 15:54:18 +0300

    up

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <template>
-  <div id="app">    
-    <h1 class="title">Время в некоторых городах мира:</h1> 
-    <TimeWorld
-      v-for = "(time, keys) of timers"
-      :key="keys"
-      :region="time.region"
-      :utc="time.utc" />    
+  <div id="app">
+    <h1 class="title">"And We Knew It Was Our Time...""</h1>
+    <TimeWorld v-for="(time, keys) of timers" :key="keys" :region="time.region" :utc="time.utc" />
   </div>
 </template>
 
@@ -14,32 +10,33 @@ import TimeWorld from './components/TimeWorld.vue'
 
 export default {
   name: 'App',
-  data () {
-    return {       
+  data() {
+    return {
       timers: [
-        {region: 'Всемирное координированное время (UTC): ', utc: 0},
-        {region: 'Украина, Киев (+2 часа): ', utc: 120},
-        {region: 'США, Кливленд (-5 часов): ', utc: -300},
-        {region: 'Япония, Токио (+9 часов): ', utc: 540},
-        {region: 'Великобритания , Лондон (0 часов): ', utc: 0},
-        {region: 'Австралия , Сидней (+11 часов): ', utc: 660}         
+        { region: 'Coordinated Universal Time (UTC): ', utc: 0 },
+        { region: 'Ukraine, Kyiv (+2 hours): ', utc: 120 },
+        { region: 'USA, Cleveland (-5 hours): ', utc: -300 },
+        { region: 'Japan, Tokyo (+9 hours): ', utc: 540 },
+        { region: 'United Kingdom , London (0 hours): ', utc: 0 },
+        { region: 'Australia, Sydney (+11 hours): ', utc: 660 }
       ]
     }
   },
   components: {
     TimeWorld
-  }   
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;   
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
-.title{
+
+.title {
   color: coral;
 }
 </style>
--- src/components/TimeWorld.vue
+++ src/components/TimeWorld.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="wrap">    
-    <span>{{ region }}</span> 
-    <span>{{ strDate }}</span>     
-    <button v-on:click="handleClick" class="btn">{{ marker ? 'STOP' : 'START' }}</button>       
+  <div class="wrap">
+    <span>{{ region }}</span>
+    <span>{{ strDate }}</span>
+    <button v-on:click="handleClick" class="btn">{{ marker ? 'STOP' : 'START' }}</button>
   </div>
 </template>
 
@@ -10,73 +10,75 @@
 export default {
   name: 'TimeWorld',
   props: ['region', 'utc'],
-  created(){     
+  created() {
     this.tick();
   },
-  destroyed(){
+  destroyed() {
     clearTimeout(this.time);
   },
-  data(){
+  data() {
     return {
       strDate: this.dateUtc(),
       time: '',
       strBtn: 'Stop',
-      marker: true      
+      marker: true
     }
   },
-  methods:{
-    dateUtc(){   
+  methods: {
+    dateUtc() {
       let date = new Date();
       let y = date.getUTCFullYear();
       let m = date.getUTCMonth();
       let d = date.getUTCDate();
-      let h = date.getUTCHours();      
-      let mn = date.getUTCMinutes() + Number(this.utc);      
+      let h = date.getUTCHours();
+      let mn = date.getUTCMinutes() + Number(this.utc);
       let s = date.getUTCSeconds();
-      let newDate = new Date(y, m, d, h , mn, s);
-      this.strDate = newDate.toLocaleString();           
-    }, 
-    tick(){
-      this.time = setInterval(()=> {
-        this.dateUtc();          
-      },1000);
-    }, 
-    handleClick(e){
+      let newDate = new Date(y, m, d, h, mn, s);
+      this.strDate = newDate.toLocaleString();
+    },
+    tick() {
+      this.time = setInterval(() => {
+        this.dateUtc();
+      }, 1000);
+    },
+    handleClick(e) {
       this.marker = !this.marker;
-      if (e.target.innerHTML === 'STOP'){
+      if (e.target.innerHTML === 'STOP') {
         clearTimeout(this.time);
       }
-      else{
-        this.time = setInterval(()=> {
-          this.dateUtc();          
-        },1000);
-      }     
+      else {
+        this.time = setInterval(() => {
+          this.dateUtc();
+        }, 1000);
+      }
     }
-  }   
+  }
 }
 </script>
- 
+
 <style scoped>
-.wrap{    
-  border: 1px solid ;
+.wrap {
+  border: 1px solid;
   border-radius: 5px;
   padding: 20px;
   width: 80%;
   max-width: 550px;
   margin: 0 auto 20px auto;
   box-shadow:
-  0 1px 4px rgba(0, 0, 0, .3),
-  -23px 0 20px -23px rgba(0, 0, 0, .8),
-  23px 0 20px -23px rgba(0, 0, 0, .8),
-  0 0 40px rgba(0, 0, 0, .1) inset;
+    0 1px 4px rgba(0, 0, 0, .3),
+    -23px 0 20px -23px rgba(0, 0, 0, .8),
+    23px 0 20px -23px rgba(0, 0, 0, .8),
+    0 0 40px rgba(0, 0, 0, .1) inset;
 }
+
 .wrap:hover,
 .btn:hover {
   cursor: pointer;
   font-weight: bold;
-} 
-.btn{
+}
+
+.btn {
   display: block;
   margin: 10px auto 0 auto;
-} 
+}
 </style>
